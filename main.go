27048def3960a17ebaea743b04006781b758999c
Thomas <thomas.chastaingt@gmail.com>
2023-05-14 21:28:11 +0200

    manage transport layer

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+
+	"github.com/chastaingt/tblock/network"
+)
+
+func main() {
+	trlocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trlocal.Connect(trRemote)
+	trRemote.Connect(trlocal)
+
+	go func() {
+		for {
+			trRemote.SendMessage(trlocal.Addr(), []byte("hello world"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trlocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
+}
